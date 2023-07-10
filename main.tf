fdb8cf52ed348a0765f66053195b4f034d843cab
BALA788799 <139132194+BALA788799@users.noreply.github.com>
2023-07-10 18:12:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,3 +65,72 @@ resource "aws_cloudwatch_dashboard" "s2s_vpn_dashboard" {
     }
   EOF
 }
+
+
+resource "aws_cloudwatch_dashboard" "vpn_tunnel_dashboard" {
+  dashboard_name = "VPN Tunnel Dashboard"
+  dashboard_body = <<EOF
+    {
+      "widgets": [
+        {
+          "type": "text",
+          "x": 0,
+          "y": 0,
+          "width": 24,
+          "height": 1,
+          "properties": {
+            "markdown": "## VPN Tunnel Status"
+          }
+        },
+        {
+          "type": "metric",
+          "x": 0,
+          "y": 1,
+          "width": 12,
+          "height": 8,
+          "properties": {
+            "view": "timeSeries",
+            "stacked": false,
+            "metrics": [
+              [ "AWS/VPN", "TunnelState", "VpnId", aws_vpn_connection.example.id ]
+            ],
+            "region": "us-west-2",
+            "title": "Tunnel State",
+            "stat": "Maximum",
+            "period": 300,
+            "yAxis": {
+              "left": {
+                "label": "State",
+                "showUnits": false
+              }
+            }
+          }
+        },
+        {
+          "type": "metric",
+          "x": 12,
+          "y": 1,
+          "width": 12,
+          "height": 8,
+          "properties": {
+            "view": "timeSeries",
+            "stacked": false,
+            "metrics": [
+              [ "AWS/VPN", "TunnelDataIn", "VpnId", aws_vpn_connection.example.id, { "stat": "Average" } ],
+              [ "AWS/VPN", "TunnelDataOut", "VpnId", aws_vpn_connection.example.id, { "stat": "Average" } ]
+            ],
+            "region": "us-west-2",
+            "title": "VPN Traffic",
+            "period": 300,
+            "yAxis": {
+              "left": {
+                "label": "Traffic",
+                "showUnits": true
+              }
+            }
+          }
+        }
+      ]
+    }
+  EOF
+}
